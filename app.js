8a9eba2adb4320c8404fefe120d195541d311338
Shubham <shubhamrockmail@gmail.com>
2021-06-07 01:37:32 +0530

    final release

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8000;
+const bodyparser = require("body-parser")
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+  const Contact = mongoose.model('Contact', contactSchema);
 
 
 // EXPRESS SPECIFIC STUFF
@@ -21,6 +35,15 @@ app.get('/', (req, res) => {
 app.get('/contact', (req, res) => {
     const params = {  }
     res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res) => {
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been save to the database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    });
 })
 
 
